69edbc7891dbef740a10eedc4d8abc31ba2f1199
Michael Wixson <wixsonmc@gmail.com>
2020-02-18 13:44:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -184,5 +184,17 @@ def player_stats (players_name)
 end
 
 def big_shoe_rebounds
-  
+  biggest_shoe = 0
+  num_rebounds = 0
+
+  game_hash.each do |_team, game_data|
+    game_data[:players].each do |player|
+      if player[:shoe] > biggest_shoe
+        biggest_shoe = player[:shoe]
+        num_rebounds = player[:rebounds]
+      end
+    end
+  end
+
+  num_rebounds
 end
