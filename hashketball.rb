36434a8c8e3e705c1ba542b5fe3ad5160230497f
Michael Wixson <wixsonmc@gmail.com>
2020-02-18 13:35:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -171,6 +171,6 @@ def player_stats (players_name)
     team.each do |attributes, data|
       if attributes == :players
         data.each do |player|
-          if player[:players_name] == players_name
+          if player[:player_name] == players_name
             
 end
